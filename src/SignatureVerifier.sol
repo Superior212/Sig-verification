1038b8c4cb2df7796d83479ca7fdd335f15127d9
Superior <samsonaderonmu2021@gmail.com>
2024-10-18 19:57:29 +0100

    Refactor import statements in SignatureVerifier.sol and ECDSA.sol

--- src/SignatureVerifier.sol
+++ src/SignatureVerifier.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.24;
 
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "./libary/ERC20.sol";
+import "./libary/ECDSA.sol";
 
 
 
